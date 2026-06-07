3221bc8d084d92ce4d9ee0ce3f8b2fc124d02add
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-06-07 22:35:34 +0530

    key

--- langchain/one.py
+++ langchain/one.py
@@ -1,13 +1,6 @@
-from dotenv import load_dotenv
+"""from dotenv import load_dotenv
 import os
-from langchain_openai import OpenAI 
-
-
 load_dotenv()
-"""
-llm=ChatOpenAI()
-model.invoke"""
 
-lm=OpenAI()
-lm.invoke("tel me about jagan")
+print(os.getenv("OPENAI_API_KEY"))"""
 
