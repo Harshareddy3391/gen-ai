b26052581622cdcaf38418c4c0edcc3e996bb463
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-06-09 21:50:16 +0530

    Ignore .env file

--- langchain/one.py
+++ langchain/one.py
@@ -1,6 +1,41 @@
-"""from dotenv import load_dotenv
-import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
 load_dotenv()
 
-print(os.getenv("OPENAI_API_KEY"))"""
+client = OpenAI()
 
+messages = []
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    messages.append(
+        {"role": "user", "content": user_input}
+    )
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=messages
+    )
+
+    ai_reply = response.choices[0].message.content
+
+    print("AI:", ai_reply)
+
+    messages.append(
+        {"role": "assistant", "content": ai_reply}
+    )
+
+"""
+
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(model="llama3")
+
+res = llm.invoke("Tell me about Andhra Pradesh")
+
+print(res.content)"""
