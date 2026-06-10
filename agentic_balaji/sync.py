e783ada2ffab9b6156b2399d067180b9a805d095
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-06-11 00:00:46 +0530

    sync

--- agentic_balaji/sync.py
+++ agentic_balaji/sync.py
@@ -1,14 +1,20 @@
 import time
-def sy_fun(city):
 
-    print(f"wait whether searching in {city}")
+def get_w(city):
+
+    print(f"whether wait {city}")
     time.sleep(2)
-    return f"is cool in {city}"
-start=time.time()
+    return  f"{city}is cool......"
 
-li=["banglore","chennai"]
-for i in li:
-    res=sy_fun(i)
+
+s=time.time()
+a=["bang","punr","hyd"]
+
+for i in a:
+
+    res=get_w(i)
     print(res)
 
-print(f"time :{round(time.time()-start,2)}")
+e=time.time()
+
+print(f"time: {round(e-s)} seconds")
