55cefd7cbcec4df8e30d7243f1c04359ef5ed28f
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-06-19 10:41:52 +0530

    jun19

--- langchain/loadbalance.py
+++ langchain/loadbalance.py
@@ -1,27 +1,8 @@
-arry_data=[1,1,1,2,3,4,5,4,3,4,6,43,3,3]
-val_data={} 
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 
-for i in arry_data:
+load_dotenv()
 
-    if i in val_data:
-        val_data[i]+=1
-    else:
-        val_data[i]=1
-
-print(val_data)
-"""
-val=[]
-for i,num_d in val_data.items():
-    val.append(num_d)
-
-for j,num in val_data.items():
-    if max(val) == num:
-        print(j)"""
-
-list_a=max(list(val_data.values()))
-for i,num in val_data.items():
-    if list_a == num:
-        print(i)
-
-
- 
+ll=ChatOpenAI()
+res=ll.invoke("hi i am uday")
+print(res.content)
